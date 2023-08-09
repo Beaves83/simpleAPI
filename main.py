23ca948e3d43bde0d1b5d49ee378880c4542686c
Bibi <beaves83@gmail.com>
2023-08-09 21:16:06 +0200

    feat: create initial file with basic API integration

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+#Url local: http://127.0.0.1:8000
+
+@app.get("/") #Esto es para el contexto de fastapi
+async def root(): #Esto es una función de python puro
+    return "Hello world!" #Todas las funciones al servidor tienes que ser asincronas
+
+@app.get("/url")
+async def url():
+    return { "url": "https://web.es" }
+
+#Inicia el server: uvicorn main:app --reload
+
+#Documentación con Swagger: http://127.0.0.1:8000/docs
+#Documentación con Redocly: http://127.0.0.1:8000/redoc
