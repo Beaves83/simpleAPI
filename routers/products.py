30f7ea8347d301e0c4fd4b68f3163d8e5a5e516b
Bibi <beaves83@gmail.com>
2023-08-09 22:53:01 +0200

    feat: add APIRouter with prefix and tags for products endpoints

--- routers/products.py
+++ routers/products.py
@@ -1,21 +1,16 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 
-router = APIRouter()
+router = APIRouter(prefix="/products", 
+                   tags=["products"],
+                   responses={404: {"message": "No encontrado"}})
 
-class User(BaseModel): #Usando BaseModel, crea por debajo toda la estructura de una clase como el constructor
-    id: int
-    name: str
-    surname: str
-    url: str
-    age: int
-
-users_list = [User(id=1, name = "Bibi", surname= "Ruiz", url ="bruiz.es", age=39 ),
-           User(id=2, name = "Cristina", surname= "Usero", url ="cusero.es", age=36 ),
-           User(id=3, name = "Alvaro", surname= "Ruiz", url ="aruiz.es", age=8 ),
-           User(id=4, name = "Martin", surname= "Ruiz", url ="mruiz.es", age=4 )]
+products_list = ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5"]
 
 
-@router.get("/products")
+@router.get("/")
 async def products():
-    return ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5"]
+    return products_list
+
+@router.get("/{id}")
+async def products(id: int):
+    return products_list[id]
