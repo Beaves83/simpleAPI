3ef83c0a374a28107425fd2c209b7c304c6f0f6a
Bibi <beaves83@gmail.com>
2023-08-17 19:56:19 +0200

    refactor: change the route of db_client

--- db/client.py
+++ db/client.py
@@ -1,4 +1,12 @@
+### MongoDB client ###
+
+# Descarga versión community: https://www.mongodb.com/try/download
+# Instalación:https://www.mongodb.com/docs/manual/tutorial
+# Módulo conexión MongoDB: pip install pymongo
+# Ejecución: sudo mongod --dbpath "/path/a/la/base/de/datos/"
+# Conexión: mongodb://localhost
+
 from pymongo import MongoClient
 
-db_client = MongoClient()
+db_client = MongoClient().local
 
